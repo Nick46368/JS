794af5bd36a5f4382c5979ca8e2750cf21b38012
Nick <48726657+Nick46368@users.noreply.github.com>
2020-09-04 15:35:02 +0300

    task seems like finished

--- Classes/triangleArea.js
+++ Classes/triangleArea.js
@@ -5,70 +5,55 @@ class Point {
     }
 }
 
-class Triangle {
-    constructor(point1, point2, point3) {
-        this.point1 = point1;
-        this.point2  = point2;
-        this.point3 = point3;
-    }
-
-    triangleArea() {
-      if (typeof this.point1.x1 === "number" && typeof this.point2.x2 === "number" 
-      && typeof this.point3.x3 === "number" && typeof this.point1.y1 === "number" 
-      && typeof this.point2.y2 === "number" && typeof this.point3.y3 === "number") {
-          
-        return 0.5 * (this.point1.x1 * this.point2.y2 + this.point2.x2 * this.point3.y3 + this.point3.x3 * this.point1.y1 
-            - this.point2.x2 * this.point1.y1 - this.point3.x3 * this.point2.y2 - this.point1.x1 * this.point3.y3)*(-1);
-      // let distanceBetweenP1AndP2 = Math.sqrt((this.point2.x2 - this.point1.x1) ** 2 + (this.point2.y2 - this.point1.y1));
-
-      // let distanceBetweenP2AndP3 = Math.sqrt((this.point3.x3 - this.point2.x2) ** 2 + (this.point3.y3 - this.point2.y2));
-
-      // let distanceBetweenP3AndP1 = Math.sqrt((this.point1.x1 - this.point3.x3) ** 2 + (this.point1.y1 - this.point3.y3));
-
-      // let halfOfThePerimetr = (distanceBetweenP1AndP2 + distanceBetweenP2AndP3 + distanceBetweenP3AndP1) / 2;
-
-      // return Math.sqrt(halfOfThePerimetr * (halfOfThePerimetr - distanceBetweenP1AndP2) * (halfOfThePerimetr - distanceBetweenP2AndP3) * (halfOfThePerimetr - distanceBetweenP3AndP1));
-    } else { 
-    return "Exception: the area could not be calculated"
-    }
-    }
-}
-
-let newTriangle = new Triangle({x1: 1, y1: 2}, {x2: 2, y2: 10}, {x3: 16, y3: -10});
-console.log(newTriangle.triangleArea());
-
-
---------------------------------------------------------------
-class Point {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-    }
-}
-
 class Triangle extends Point {
-    constructor(x) {
-      super(x);
-    }
+  constructor(x) {
+    super(x);
+  }
 
-    triangleArea() {
-      /*if (this.point1 !==0 && this.point2 !==0 && this.point3 !==0){ */
-      let distanceBetweenP1AndP2 = Math.sqrt((this.secondPoint.x - this.firstPoint.x) ** 2 + (this.secondPoint.y - this.firstPoint.y));
+    triangleGaussArea() {
 
-      let distanceBetweenP2AndP3 = Math.sqrt((this.point3.x3 - this.point2.x2) ** 2 + (this.point3.y3 - this.point2.y2));
-
-      let distanceBetweenP3AndP1 = Math.sqrt((this.point1.x1 - this.point3.x3) ** 2 + (this.point1.y1 - this.point3.y3));
-
-      let halfOfThePerimetr = (distanceBetweenP1AndP2 + distanceBetweenP2AndP3 + distanceBetweenP3AndP1) / 2;
-
-      return Math.sqrt(halfOfThePerimetr*(halfOfThePerimetr - distanceBetweenP1AndP2) * (halfOfThePerimetr - distanceBetweenP2AndP3) * (halfOfThePerimetr - distanceBetweenP3AndP1));
-/*    } else { 
-    return "Exception: the area could not be calculated" */
-    }
+      if (typeof firstPoint.x === "number" && typeof secondPoint.x === "number" && typeof thirdPoint.x === "number" && typeof firstPoint.y === "number" && typeof secondPoint.y === "number" && typeof thirdPoint.y === "number") {
+      
+        return 0.5 * (firstPoint.x * secondPoint.y + secondPoint.x * thirdPoint.y + thirdPoint.x * firstPoint.y - secondPoint.x * firstPoint.y - thirdPoint.x * secondPoint.y - firstPoint.x * thirdPoint.y)*(-1);
+        } else { 
+          return "Exception: the area could not be calculated"
+        }
     }
 }
+
 let firstPoint = new Point(1, 2);
 let secondPoint = new Point(2, 10);
 let thirdPoint = new Point(16, -10)
 let newTriangle = new Triangle(firstPoint, secondPoint, thirdPoint);
-console.log(newTriangle.triangleArea());
+console.log(newTriangle.triangleGaussArea());
+
+// =============================================================================
+// class Point {
+//     constructor(x, y) {
+//         this.x = x;
+//         this.y = y;
+//     }
+// }
+
+// class Triangle {
+//     constructor(point1, point2, point3) {
+//         this.point1 = point1;
+//         this.point2  = point2;
+//         this.point3 = point3;
+//     }
+//     triangleGaussArea() {
+
+//       if (typeof this.point1.x1 === "number" && typeof this.point2.x2 === "number" 
+//         && typeof this.point3.x3 === "number" && typeof this.point1.y1 === "number" 
+//         && typeof this.point2.y2 === "number" && typeof this.point3.y3 === "number") {
+
+//         return 0.5 * (this.point1.x1 * this.point2.y2 + this.point2.x2 * this.point3.y3 + this.point3.x3 * this.point1.y1 
+//             - this.point2.x2 * this.point1.y1 - this.point3.x3 * this.point2.y2 - this.point1.x1 * this.point3.y3)*(-1);
+//         } else { 
+//             return "Exception: the area could not be calculated"
+//          }
+//     }
+// }
+
+// let newTriangle = new Triangle({x1: 1, y1: 2}, {x2: 2, y2: 10}, {x3: 16, y3: -10});
+// console.log(newTriangle.triangleGaussArea());
