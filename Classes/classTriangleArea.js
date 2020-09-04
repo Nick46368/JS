1fc17e51a8ee4662624c173caad01d0466048b20
Nick <48726657+Nick46368@users.noreply.github.com>
2020-09-04 21:09:04 +0300

    delete trash from Triangle

--- Classes/classTriangleArea.js
+++ Classes/classTriangleArea.js
@@ -28,34 +28,3 @@ let secondPoint1 = new Point(2, 10);
 let thirdPoint1 = new Point(16, -10);
 let newTriangle = new Triangle(firstPoint1, secondPoint1, thirdPoint1);
 console.log(newTriangle.triangleGaussArea());
-
-// =============================================================================
-// class Point {
-//     constructor(x, y) {
-//         this.x = x;
-//         this.y = y;
-//     }
-// }
-
-// class Triangle {
-//     constructor(point1, point2, point3) {
-//         this.point1 = point1;
-//         this.point2  = point2;
-//         this.point3 = point3;
-//     }
-//     triangleGaussArea() {
-
-//       if (typeof this.point1.x1 === "number" && typeof this.point2.x2 === "number" 
-//         && typeof this.point3.x3 === "number" && typeof this.point1.y1 === "number" 
-//         && typeof this.point2.y2 === "number" && typeof this.point3.y3 === "number") {
-
-//         return 0.5 * (this.point1.x1 * this.point2.y2 + this.point2.x2 * this.point3.y3 + this.point3.x3 * this.point1.y1 
-//             - this.point2.x2 * this.point1.y1 - this.point3.x3 * this.point2.y2 - this.point1.x1 * this.point3.y3)*(-1);
-//         } else { 
-//             return "Exception: the area could not be calculated"
-//          }
-//     }
-// }
-
-// let newTriangle = new Triangle({x1: 1, y1: 2}, {x2: 2, y2: 10}, {x3: 16, y3: -10});
-// console.log(newTriangle.triangleGaussArea());
