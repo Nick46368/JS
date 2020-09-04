80701b9e1e54300d992d6cd2244fcfb0abca023a
Nick <48726657+Nick46368@users.noreply.github.com>
2020-09-04 17:47:00 +0300

    final

--- Classes/triangleArea.js
+++ Classes/triangleArea.js
@@ -1,3 +1,4 @@
+"use strict"
 class Point {
     constructor(x, y) {
         this.x = x;
@@ -5,31 +6,27 @@ class Point {
     }
 }
 
-class Figure extends Point{
-  constructor(x) {
-    super(x);
-  }
-}
-
-class Triangle extends Figure {
-  constructor(x) {
-    super(x);
+class Triangle {
+  constructor(firstPoint, secondPoint, thirdPoint) {
+    this.firstPoint = firstPoint;
+    this.secondPoint = secondPoint;
+    this.thirdPoint = thirdPoint;
   }
 
     triangleGaussArea() {
 
-      if (typeof firstPoint.x === "number" && typeof secondPoint.x === "number" && typeof thirdPoint.x === "number" && typeof firstPoint.y === "number" && typeof secondPoint.y === "number" && typeof thirdPoint.y === "number") {
+      if (typeof this.firstPoint.x === "number" && typeof this.secondPoint.x === "number" && typeof this.thirdPoint.x === "number" && typeof this.firstPoint.y === "number" && typeof this.secondPoint.y === "number" && typeof this.thirdPoint.y === "number") {
       
-        return 0.5 * (firstPoint.x * secondPoint.y + secondPoint.x * thirdPoint.y + thirdPoint.x * firstPoint.y - secondPoint.x * firstPoint.y - thirdPoint.x * secondPoint.y - firstPoint.x * thirdPoint.y)*(-1);
+        return 0.5 * (this.firstPoint.x * this.secondPoint.y + this.secondPoint.x * this.thirdPoint.y + this.thirdPoint.x * this.firstPoint.y - this.secondPoint.x * this.firstPoint.y - this.thirdPoint.x * this.secondPoint.y - this.firstPoint.x * this.thirdPoint.y)*(-1);
         } else { 
           return "Exception: the area could not be calculated"
         }
     }
 }
-let firstPoint = new Point(1, 2);
-let secondPoint = new Point(2, 10);
-let thirdPoint = new Point(16, -10)
-let newTriangle = new Triangle(firstPoint, secondPoint, thirdPoint);
+let firstPoint1 = new Point(1, 2);
+let secondPoint1 = new Point(2, 10);
+let thirdPoint1 = new Point(16, -10);
+let newTriangle = new Triangle(firstPoint1, secondPoint1, thirdPoint1);
 console.log(newTriangle.triangleGaussArea());
 
 // =============================================================================
